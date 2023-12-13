e53118139ffd3516cd910d390c52c8393e6aa069
Ankit Soni <ankit.soni@chef.io>
2023-12-13 16:03:37 +0530

    knife setup

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -2,7 +2,7 @@ sudo mkdir -p ~/.chef
 sudo tee cat ~/.chef/credentials > /dev/null <<EOT
 [default]
 client_name     = 'chefadmin'
-client_key      = '~/.chef/chefadmin.pem'
+client_key      = '/home/runner/.chef/chefadmin.pem'
 chef_server_url = 'https://ec2-13-127-199-32.ap-south-1.compute.amazonaws.com/organizations/qa'
 EOT
 sudo cat ~/.chef/credentials
