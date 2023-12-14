9f82bd428af5378f956bb1ec715e4c5c32de2834
Ankit Soni <ankit.soni@chef.io>
2023-12-14 23:51:09 +0530

    org logic

--- .github/workflows/setup_workstation.sh
+++ .github/workflows/setup_workstation.sh
@@ -14,21 +14,21 @@ json_data='{
 }'
 
 # Output file location
-output_file="$HOME/chef_config.conf"
+output_dir="$HOME/chef_configs"
 
-# Function to create the configuration file
-create_config_file() {
-  echo "[default]" > "$1"
-  echo "client_name     = \"$client_name\"" >> "$1"
-  echo "client_key      = '/home/runner/.chef/$client_key_name.pem'" >> "$1"
-  echo "chef_server_url = 'https://ec2-13-127-199-32.ap-south-1.compute.amazonaws.com/organizations/$org_name'" >> "$1"
-}
+# Create output directory if it doesn't exist
+mkdir -p "$output_dir"
 
 # Parse JSON and extract values into variables in a loop
 echo "$json_data" | jq -r 'to_entries[] | "\(.key):client_name=\(.value.client_name)\nclient_key_name=\(.value.client_key_name)\norg_name=\(.value.org_name)\n"' | while read -r line; do
   eval "$line"
-  create_config_file "$output_file.$org_name"
+  
+  # Create the configuration file
+  config_file="$output_dir/chef_config_$org_name.conf"
+  echo "[default]" > "$config_file"
+  echo "client_name     = \"$client_name\"" >> "$config_file"
+  echo "client_key      = '/home/runner/.chef/$client_key_name.pem'" >> "$config_file"
+  echo "chef_server_url = 'https://ec2-13-127-199-32.ap-south-1.compute.amazonaws.com/organizations/$org_name'" >> "$config_file"
+  
+  echo "Configuration file created: $config_file"
 done
-
-echo "Configuration files created. Output files: $output_file.*"
-cat $HOME/chef_config.conf
