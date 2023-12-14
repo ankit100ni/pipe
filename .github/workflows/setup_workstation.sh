b55c516d43a791333b89c66727e49a4ec951b867
Ankit Soni <ankit.soni@chef.io>
2023-12-15 01:03:51 +0530

    org logic

--- .github/workflows/setup_workstation.sh
+++ .github/workflows/setup_workstation.sh
@@ -16,6 +16,7 @@ mkdir -p "$output_dir"
 echo "$json_data" | jq -r 'to_entries[] | "\(.key) \(.value.client_name) \(.value.client_key_name) \(.value.org_name)"' | while read -r org client_name client_key_name org_name; do
   # Create the configuration file
   config_file="$output_dir/credentials"
+  client_key_name=echo $client_key_name | tr '[:upper:]' '[:lower:]'
   echo "[default]" > "$config_file"
   echo "client_name     = \"$client_name\"" >> "$config_file"
   echo "client_key      = '/home/runner/.chef/$client_key_name.pem'" >> "$config_file"
