d0eb9ca03970e710a4654134ea7aaecd7b7c0b86
Ankit Soni <ankit.soni@chef.io>
2023-12-15 02:09:17 +0530

    org logic

--- .github/workflows/setup_workstation.sh
+++ .github/workflows/setup_workstation.sh
@@ -4,15 +4,15 @@ json_data=$1
 CHEFADMIN=$2
 TESTUSER=$3
 # Output directory location
-output_dir="/home/runner/.chef"
+output_dir="$HOME/.chef"
 COUNTER=1
 
 # Create output directory if it doesn't exist
 mkdir -p "$output_dir"
 
 # Creating the pem file
-echo "$CHEFADMIN" > /home/runner/.chef/chefadmin.pem
-echo "$TESTUSER" > /home/runner/.chef/testuser.pem
+echo "$CHEFADMIN" > $HOME/.chef/chefadmin.pem
+echo "$TESTUSER" > $HOME/.chef/testuser.pem
 # Parse JSON and extract values into variables in a loop
 echo "$json_data" | jq -r 'to_entries[] | "\(.key) \(.value.client_name) \(.value.client_key_name) \(.value.org_name)"' | while read -r org client_name client_key_name org_name; do
   # Create the configuration file
@@ -20,12 +20,12 @@ echo "$json_data" | jq -r 'to_entries[] | "\(.key) \(.value.client_name) \(.valu
   client_key_name_small=$(echo $client_key_name | tr '[:upper:]' '[:lower:]')
   echo "[default]" > "$config_file"
   echo "client_name     = \"$client_name\"" >> "$config_file"
-  echo "client_key      = '/home/runner/.chef/$client_key_name_small.pem'" >> "$config_file"
+  echo "client_key      = '$HOME/.chef/$client_key_name_small.pem'" >> "$config_file"
   echo "chef_server_url = 'https://ec2-3-109-213-111.ap-south-1.compute.amazonaws.com/organizations/$org_name'" >> "$config_file"
   
   cat $config_file
   
-  sudo ls -lhrt /home/runner/.chef
+  sudo ls -lhrt $HOME/.chef
   knife ssl fetch
   knife ssl check
   echo " $COUNTER "
