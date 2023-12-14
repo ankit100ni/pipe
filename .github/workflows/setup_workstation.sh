813a85837515e009cc4b31a1c3b4fb929b3e502f
Ankit Soni <ankit.soni@chef.io>
2023-12-15 01:53:10 +0530

    org logic

--- .github/workflows/setup_workstation.sh
+++ .github/workflows/setup_workstation.sh
@@ -10,7 +10,8 @@ output_dir="/home/runner/.chef"
 mkdir -p "$output_dir"
 
 # Creating the pem file
-
+echo "$CHEFADMIN" > /home/runner/.chef/chefadmin.pem
+echo "$TESTUSER" > /home/runner/.chef/testuser.pem
 # Parse JSON and extract values into variables in a loop
 echo "$json_data" | jq -r 'to_entries[] | "\(.key) \(.value.client_name) \(.value.client_key_name) \(.value.org_name)"' | while read -r org client_name client_key_name org_name; do
   # Create the configuration file
@@ -19,9 +20,11 @@ echo "$json_data" | jq -r 'to_entries[] | "\(.key) \(.value.client_name) \(.valu
   echo "[default]" > "$config_file"
   echo "client_name     = \"$client_name\"" >> "$config_file"
   echo "client_key      = '/home/runner/.chef/$client_key_name_small.pem'" >> "$config_file"
-  echo "chef_server_url = 'https://ec2-13-127-199-32.ap-south-1.compute.amazonaws.com/organizations/$org_name'" >> "$config_file"
+  echo "chef_server_url = 'https://ec2-3-109-213-111.ap-south-1.compute.amazonaws.com	/organizations/$org_name'" >> "$config_file"
   
   cat $config_file
-  echo "$CHEFADMIN" > /home/runner/.chef/chefadmin.pem
+  
   sudo ls -lhrt /home/runner/.chef
+  knife ssl fetch
+  knife ssl check
 done
