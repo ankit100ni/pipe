ef483b806755b4d47c258030d7a3d5620b33cfd2
Ankit Soni <ankit.soni@chef.io>
2023-12-14 23:47:27 +0530

    org logic

--- .github/workflows/setup_workstation.sh
+++ .github/workflows/setup_workstation.sh
@@ -13,25 +13,22 @@ json_data='{
   }
 }'
 
+# Output file location
+output_file="~/.chef/credentials"
+
+# Function to create the configuration file
+create_config_file() {
+  echo "[default]" > "$1"
+  echo "client_name     = \"$client_name\"" >> "$1"
+  echo "client_key      = '/home/runner/.chef/$client_key_name.pem'" >> "$1"
+  echo "chef_server_url = 'https://ec2-13-127-199-32.ap-south-1.compute.amazonaws.com/organizations/$org_name'" >> "$1"
+}
+
 # Parse JSON and extract values into variables in a loop
-echo "$json_data" | jq -r 'to_entries[] | "\(.key): Client Name: \(.value.client_name), Client Key Name: \(.value.client_key_name), Org Name: \(.value.org_name)"' | while read -r line; do
-  org_key=$(echo "$line" | cut -d':' -f1)
-  client_name=$(echo "$line" | grep -o 'Client Name: [^,]*' | cut -d' ' -f3-)
-  client_key_name=$(echo "$line" | grep -o 'Client Key Name: [^,]*' | cut -d' ' -f4-)
-  org_name=$(echo "$line" | grep -o 'Org Name: [^,]*' | cut -d' ' -f3-)
-
-  echo "Org Key: $org_key"
-  echo "Client Name: $client_name"
-  echo "Client Key Name: $client_key_name"
-  echo "Org Name: $org_name"
-
-  sudo tee cat ~/.chef/credentials > /dev/null <<EOT
-  [default]
-  client_name     = "$client_name"
-  client_key      = "/home/runner/.chef/$client_key_name.pem"
-  chef_server_url = "https://ec2-13-127-199-32.ap-south-1.compute.amazonaws.com/organizations/$org_name"
-  EOT
-  sudo cat ~/.chef/credentials
-  sudo echo "setup.sh executed"
-  echo "---"
+echo "$json_data" | jq -r 'to_entries[] | "\(.key):client_name=\(.value.client_name)\nclient_key_name=\(.value.client_key_name)\norg_name=\(.value.org_name)\n"' | while read -r line; do
+  eval "$line"
+  create_config_file "$output_file.$org_name"
 done
+
+echo "Configuration files created. Output files: $output_file.*"
+cat ~/.chef/credentials
