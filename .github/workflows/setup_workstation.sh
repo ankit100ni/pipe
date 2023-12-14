0725eeeb610b7a71e4c766d9451b7539232f4a48
Ankit Soni <ankit.soni@chef.io>
2023-12-14 23:20:39 +0530

    org logic

--- .github/workflows/setup_workstation.sh
+++ .github/workflows/setup_workstation.sh
@@ -13,18 +13,7 @@ json_data='{
   }
 }'
 
-# Use jq to get the keys of the JSON object
-org_keys=($(echo "$json_data" | jq -r 'keys_unsorted[]'))
-
-# Iterate over the keys
-for org_key in "${org_keys[@]}"; do
-  client_name=$(echo "$json_data" | jq -r ".[$org_key].client_name")
-  client_key_name=$(echo "$json_data" | jq -r ".[$org_key].client_key_name")
-  org_name=$(echo "$json_data" | jq -r ".[$org_key].org_name")
-
-  echo "Org Key: $org_key"
-  echo "Client Name: $client_name"
-  echo "Client Key Name: $client_key_name"
-  echo "Org Name: $org_name"
-  echo "---"
+# Iterate over keys using jq
+for org_key in $(echo "$json_data" | jq -r 'keys_unsorted[]'); do
+  echo "Organization Key: $org_key"
 done
