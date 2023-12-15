7ae549285159be49c02a414d2b70d36f0c462ec7
Ankit Soni <ankit.soni@chef.io>
2023-12-15 13:59:05 +0530

    metadatacheck test

--- .github/workflows/metadatacheck.sh
+++ .github/workflows/metadatacheck.sh
@@ -6,21 +6,25 @@ IFS='.' read -ra v1_components <<< "$dev_version"
 IFS='.' read -ra v2_components <<< "$branch_version"
 
 if [ "${v1_components[0]}" -gt "${v2_components[0]}" ]; then
-    echo "$dev_version is greater than $branch_version."
+    echo "ERROR: $dev_version is greater than $branch_version."
+    exit 1
 elif [ "${v1_components[0]}" -lt "${v2_components[0]}" ]; then
     echo "$dev_version is less than $branch_version."
 else
     if [ "${v1_components[1]}" -gt "${v2_components[1]}" ]; then
-        echo "$dev_version is greater than $branch_version."
+        echo "ERROR: $dev_version is greater than $branch_version."
+        exit 1
     elif [ "${v1_components[1]}" -lt "${v2_components[1]}" ]; then
         echo "$dev_version is less than $branch_version."
     else
         if [ "${v1_components[2]}" -gt "${v2_components[2]}" ]; then
-            echo "$dev_version is greater than $branch_version."
+            echo "ERROR: $dev_version is greater than $branch_version."
+            exit 1
         elif [ "${v1_components[2]}" -lt "${v2_components[2]}" ]; then
             echo "$dev_version is less than $branch_version."
         else
-            echo "$dev_version is equal to $branch_version."
+            echo "ERROR: $dev_version is equal to $branch_version."
+            exit 1
         fi
     fi
 fi
